1430c38186c31bf91ea10cd433fe3d661cbf0ba7
kanattsu <tongknatu@gmail.com>
2023-10-27 16:01:55 +0900

    Initial Commit

---
+++ gitcooperetionsample/gitcooperetionsampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitcooperetionsampleApp.swift
+//  gitcooperetionsample
+//
+//  Created by 金津愛澄 on 2023/10/27.
+//
+
+import SwiftUI
+
+@main
+struct gitcooperetionsampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
